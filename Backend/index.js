650c757e399bd14527a05458a1b2695653aa1b3e
YourGitHubUsername <your-email@example.com>
2026-01-04 20:40:40 +0530

    add

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 const URI = process.env.MongoDBURI;
 
 // connect to mongoDB
