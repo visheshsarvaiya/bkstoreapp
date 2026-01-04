b18b46194d2291a068be2bd1a3ef62f525aaf635
YourGitHubUsername <your-email@example.com>
2026-01-04 21:19:38 +0530

    add

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import banner from "../../public/Banner.png";
+
 function Banner() {
   return (
     <>
-      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
         <div className="w-full order-2 md:order-1 md:w-1/2 mt-12 md:mt-36">
           <div className="space-y-8">
             <h1 className="text-2xl md:text-4xl font-bold">
               Hello, welcomes here to learn something{" "}
               <span className="text-pink-500">new everyday!!!</span>
             </h1>
-            <p className="text-sm md:text-xl">
-             Welcome to your one-stop online bookstore! Explore 
-              thousands of books across genres, from timeless classics to new releases. Enjoy seamless shopping, exclusive deals, and fast doorstep delivery.
 
+            <p className="text-sm md:text-xl">
+              Welcome to your one-stop online bookstore! Explore thousands of
+              books across genres, from timeless classics to new releases.
+              Enjoy seamless shopping, exclusive deals, and fast doorstep
+              delivery.
             </p>
+
             <label className="input input-bordered flex items-center gap-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -28,13 +31,15 @@ function Banner() {
               <input type="text" className="grow" placeholder="Email" />
             </label>
           </div>
+
           <button className="btn mt-6 btn-secondary">Get Started</button>
         </div>
-        <div className=" order-1 w-full mt-20 md:w-1/2">
+
+        <div className="order-1 w-full mt-20 md:w-1/2">
           <img
-            src={banner}
+            src="/Banner.png"
             className="md:w-[550px] md:h-[460px] md:ml-12"
-            alt=""
+            alt="Banner"
           />
         </div>
       </div>
